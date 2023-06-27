7519b87d060be556f1386ef10a99c130e3981f6c
DESKTOP-8ACCR2E\Luís Amorim <Luis.Amorim@newday.co.uk>
2023-06-27 13:37:53 +0100

    Implemented the Surface class that represents the area of a given Plateau.

---
+++ src/MarsRover/Surface.cs
@@ -0,0 +1,34 @@
+namespace MarsRover;
+
+public class Surface
+{
+    public Surface(int width, int height)
+    {
+        if (width < 1 || height < 1) throw new ArgumentException("The width and height must be positive integers.");
+        Width = width;
+        Height = height;
+    }
+
+    public int Height { get; }
+
+    public int Width { get; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null) return false;
+        if (obj.GetType() != GetType()) return false;
+
+        var other = (Surface)obj;
+        return other.Width == Width && other.Height == Height;
+    }
+
+    public override int GetHashCode()
+    {
+        return Width.GetHashCode() + Height.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return Width + " " + Height;
+    }
+}
