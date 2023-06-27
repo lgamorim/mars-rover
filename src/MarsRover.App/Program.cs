1a07f7b875913a69ae269c6b3c33afba99b45204
DESKTOP-8ACCR2E\Luís Amorim <Luis.Amorim@newday.co.uk>
2023-06-27 22:07:01 +0100

    Added the console App project and implemented the program that handles input from the user.

---
+++ src/MarsRover.App/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace MarsRover.App;
+
+public static class Program
+{
+    private static Plateau plateau;
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Plateau Grid (x, y):\t");
+        var input = Console.ReadLine();
+        try
+        {
+            plateau = Plateau.Define(input);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        while (true)
+        {
+            DeployRover();
+            var rover = ExplorePlateau();
+            Console.WriteLine($"#{plateau.TotalRovers} Final Position:\t{rover.Position}");
+        }
+    }
+
+    private static Rover DeployRover()
+    {
+        Console.Write($"Landing Position #{plateau.TotalRovers + 1}:\t");
+        var input = Console.ReadLine();
+        try
+        {
+            var rover = plateau.Deploy(input);
+            return rover;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return DeployRover();
+        }
+    }
+
+    private static Rover ExplorePlateau()
+    {
+        Console.Write($"Control Rover #{plateau.TotalRovers}:\t");
+        var input = Console.ReadLine();
+        try
+        {
+            var rover = plateau.Explore(input);
+            return rover;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return ExplorePlateau();
+        }
+    }
+}
